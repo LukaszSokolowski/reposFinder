646ddc883e732ae97c622e6b5658c208a7c4c9c9
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2020-03-18 01:45:49 +0100

    Remove unused parts of code

--- GitHubFetcher/ViewController.swift
+++ GitHubFetcher/ViewController.swift
@@ -9,52 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let connection = ConnectionManager()
-    var repos = [Repository]()
-
-    @IBOutlet weak var reposListTextView: UITextView!
-
     override func viewDidLoad() {
-        super.viewDidLoad()
-        reposListTextView.text = ""
+        print("Hello")
     }
-    
-    @IBAction func requestWithParamsButtonClicked(_ sender: Any) {
-        let some = connection.requestWithParams(completion: {
-            some in self.repos = some
-            var str = ""
-            if self.repos.count != 0 {
-                for item in self.repos {
-                    str = str + item.name + "\n"
-                }
-            }
-            self.reposListTextView.text = str
-            self.reposListTextView.reloadInputViews()
-            print(self.repos.count)
-        })
-        print(some)
-
-    }
-    @IBAction func connectButton(_ sender: Any) {
-        let url = "https://www.httpbin.org/get"
-        connection.cacheTextFrom(url: url)
-    }
-    @IBAction func headersButtonClicked(_ sender: Any) {
-        connection.httpHeaders()
-    }
-    @IBAction func withCredential(_ sender: Any) {
-        print("CREDENTIAL")
-        connection.authorizationWithCredential()
-        print("---------------------------")
-    }
-    @IBAction func withoutCredential(_ sender: Any) {
-        print("BASIC AUTH")
-        connection.authorizationWithoutCredential()
-        print("---------------------------")
-    }
-    @IBAction func showReposButtonClicked(_ sender: Any) {
-
-    }
-
 }
 
