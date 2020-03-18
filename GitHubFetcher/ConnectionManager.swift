4893aa0cd68b0e25c23d720545511f2ac18d7ca2
Łukasz Sokołowski <lukasz.m.sokolowski1995@gmail.com>
2020-03-18 02:04:19 +0100

    Prepare fundamental elements of app

---
+++ GitHubFetcher/ConnectionManager.swift
@@ -0,0 +1,15 @@
+//
+//  ConnectionManager.swift
+//  GitHubFetcher
+//
+//  Created by Łukasz Sokołowski on 18/03/2020.
+//  Copyright © 2020 Łukasz Sokołowski. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ConnectionManager {
+    
+}
+
---
+++ GitHubFetcher/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  GitHubFetcher
+//
+//  Created by Łukasz Sokołowski on 18/03/2020.
+//  Copyright © 2020 Łukasz Sokołowski. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
